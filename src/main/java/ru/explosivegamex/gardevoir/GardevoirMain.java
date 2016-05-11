799d14a103edef331e5a7fb212f2f5801cd84a5f
Jeki <laptew.c@yandex.ru>
2016-05-11 22:31:26 +0500

    Initial commit

---
+++ src/main/java/ru/explosivegamex/gardevoir/GardevoirMain.java
@@ -0,0 +1,12 @@
+package ru.explosivegamex.gardevoir;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GardevoirMain extends JavaPlugin
+{
+    @Override
+    public void onEnable() {
+        Bukkit.getLogger().info("[Gardevoir] Gardevoir has been enabled.");
+    }
+}
