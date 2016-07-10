f96d273287c030df6d53c6bae957f1553f933567
Jeki <laptew.c@yandex.ru>
2016-07-10 21:00:40 +0500

    Refactoring

--- src/main/java/ru/explosivegamex/gardevoir/modules/caseInformer/CaseInformer.java
+++ src/main/java/ru/explosivegamex/gardevoir/modules/caseInformer/CaseInformer.java
@@ -1,15 +1,15 @@
 package ru.explosivegamex.gardevoir.modules.caseInformer;
 
 import net.lightshard.itemcases.event.RewardReceivedEvent;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import ru.explosivegamex.gardevoir.modules.Module;
 import ru.explosivegamex.gardevoir.modules.caseInformer.fireworkScripts.SquareScript;
+import ru.explosivegamex.gardevoir.util.GardevoirPlayer;
 import ru.explosivegamex.gardevoir.util.Inform;
 import ru.explosivegamex.gardevoir.util.MessageType;
-import ru.tehkode.permissions.PermissionUser;
-import ru.tehkode.permissions.bukkit.PermissionsEx;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -28,21 +28,22 @@ public class CaseInformer extends Module implements Listener {
 
     @EventHandler
     public void on(RewardReceivedEvent e) {
-        Map<String, String> parameters = new HashMap<>();
-        parameters.put("player", e.getPlayer().getName());
-        parameters.put("privilege", getPrivilegeName(e));
-
-        Inform.toAll("Игрок %player% открыл кейс с привилегией - %privilege%. Поздравим его!", MessageType.INFO, parameters);
-        Inform.to(e.getPlayer(), "Поздравляем Вас с открытием кейса. Вы можете написать команду /menu и перейти в пункт \"Донат\", после чего найти и прочитать все возможности Вашей новой привилегии. Так же не забывайте ознакомиться с правилами для донатеров, дабы избежать неприятной ситуации."
-                , MessageType.SUCCESS);
-
-        FireworksProcessor fireworks = new FireworksProcessor(new SquareScript(e.getPlayer().getLocation()));
-        fireworks.run();
+        if (e.getItemCase().getName().equals("privilege")) {
+            runOpenCaseEffect(e.getPlayer());
+        }
     }
 
-    private String getPrivilegeName(RewardReceivedEvent e) {
-        PermissionUser pexUser = PermissionsEx.getUser(e.getPlayer());
-        return pexUser.getPrefix();
+    private void runOpenCaseEffect(Player player) {
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("player", player.getName());
+        parameters.put("privilege", GardevoirPlayer.of(player).getPrivilegeName());
+
+        Inform.toAll("Игрок %player% открыл кейс с привилегией - %privilege%. Поздравим его!", MessageType.INFO, parameters);
+        Inform.to(player, "Поздравляем Вас с открытием кейса. Вы можете написать команду /menu и перейти в пункт \"Донат\", после чего найти и прочитать все возможности Вашей новой привилегии. Так же не забывайте ознакомиться с правилами для донатеров, дабы избежать неприятной ситуации."
+                , MessageType.SUCCESS);
+
+        FireworksProcessor fireworks = new FireworksProcessor(new SquareScript(player.getLocation()));
+        fireworks.run();
     }
 
     @Override
--- src/main/java/ru/explosivegamex/gardevoir/util/GardevoirPlayer.java
+++ src/main/java/ru/explosivegamex/gardevoir/util/GardevoirPlayer.java
@@ -4,6 +4,8 @@ import com.sk89q.worldguard.bukkit.WGBukkit;
 import com.sk89q.worldguard.protection.managers.RegionManager;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import org.bukkit.entity.Player;
+import ru.tehkode.permissions.PermissionUser;
+import ru.tehkode.permissions.bukkit.PermissionsEx;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -24,6 +26,11 @@ public class GardevoirPlayer {
         Set<ProtectedRegion> regionSet = manager.getApplicableRegions(player.getLocation()).getRegions();
 
         return regionSet.stream().anyMatch(protectedRegion -> protectedRegion.getId().equals(regionName));
+    }
+
+    public String getPrivilegeName() {
+        PermissionUser pexUser = PermissionsEx.getUser(player);
+        return pexUser.getPrefix();
     }
 
     public static GardevoirPlayer of(Player player) {
